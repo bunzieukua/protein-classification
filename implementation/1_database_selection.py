0a114b24c948691c941396bf86b3f9e776042a4a
Le Pham <pnhoangle@gmail.com>
2026-05-30 02:43:06 -0700

    Update 1_database_selection.py

--- implementation/1_database_selection.py
+++ implementation/1_database_selection.py
@@ -9,34 +9,32 @@ from Bio.PDB import PDBParser
 from scipy.linalg import svd
 from Bio.PDB.MMCIFParser import MMCIFParser
 
-# Load your CIF file (downloaded from RCSB Protein Data Bank)
-cif_file = "/1dw3.cif" #file name will change based on your selected protein. here we're working with 1dw3
+# Here we work with 1ql4 protein. The file name with "1ql4" will change based on which protein you are interested in.
+# List of hydrophobic residues
+hydrophobic = {'ALA', 'VAL', 'ILE', 'LEU', 'MET', 'PHE', 'TRP', 'PRO'}
+
+# Load your CIF file
+cif_file = "/1ql4.cif"
 parser = MMCIFParser(QUIET=True)
 structure = parser.get_structure("structure", cif_file)
 
 # Initialize an empty list to store residue + atom data
-residue_1dw3 = []
+residue_1ql4 = []
 
 # Iterate over all residues and all atoms
 for model in structure:
     for chain in model:
         for residue in chain:
             resname = residue.get_resname()
-            resid = residue.get_id()[1]
-            chain_id = chain.id
-
-            for atom in residue:
-                atom_name = atom.get_name()
-                x, y, z = atom.coord
-                residue_1dw3.append({
+            if resname in hydrophobic:  # Count only hydrophobic residues
+                resid = residue.get_id()[1]
+                chain_id = chain.id
+                ca_atom = residue["CA"] if "CA" in residue else None
+                if ca_atom:
+                  x, y, z = ca_atom.coord
+                  residue_1ql4.append({
                     "residue": resname,
-                    "resid": resid,
-                    "chain": chain_id,
-                    "atom": atom_name,
                     "x": float(x),
                     "y": float(y),
                     "z": float(z)
                 })
-
-# Output or use the list
-print(residue_1dw3[:5])  # Preview first 5 entries
